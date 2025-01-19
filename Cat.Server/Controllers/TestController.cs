5c3490bcec5cd54b9b754bd6841da698222faaa7
Gleb Mihalev <mixalev702@mail.ru>
2025-01-19 16:00:21 +0300

    add SignalR

---
+++ Cat.Server/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cat.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("TestServer")]
+        public ActionResult TestServer()
+        {
+            return Ok("Всё хорошо");
+        }
+
+    }
+}
---
+++ Cat.Server/Hubs/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Cat.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string groupName, string user, string message)
+        {
+            // Отправка сообщения только в указанную группу
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task JoinGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public async Task LeaveGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+        }
+    }
+
+}
